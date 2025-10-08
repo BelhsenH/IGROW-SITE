3ce5fda7f2f5ea29ba4c19a627a2ffab94051c30
Belhsen Hosni <belhsenh@gmail.com>
2025-10-08 15:10:28 +0100

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button, ButtonDirective} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, Button, ButtonDirective],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class App {
+export class AppModule {
   protected readonly title = signal('igrow');
 }
